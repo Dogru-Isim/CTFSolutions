3b41cfbb57b6a6a46ebd0dc55b37c2e9182a345e
Dogru-Isim <96004353+Dogru-Isim@users.noreply.github.com>
2022-11-08 23:38:49 +0000

    Update getFile.sh

--- TryHackMe/getFile.sh
+++ TryHackMe/getFile.sh
@@ -8,5 +8,7 @@ echo file://$(pwd)/`wget $1 2>&1 | tail -n 2 | sed "s/.*‘//" | sed "s/’.*//"
 #  If you're taking notes (in Vim etc.) while doing TryHackMe walkthroughs and want to add local images to you notes, you can do file://<image_path>
 # When you open or view it, you can right click the url and select "open link". It will show you the image.
 
-# This script will download the image to you current directroy from the url you give it and give you the file:/// format so that you can copy
+# or just learn about [hyperlink](<image_path>) (hyperlinks in .md files)
+
+# This script will download the image to you current directroy from the url you gave it and give you in the file:/// format so that you can copy
 # and paste it to your notes to view later locally.
